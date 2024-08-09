9bcee05edd8454ed8ca88037c64850265dc89c95
Rodion Kim <skyrood2@yandex.ru>
2024-08-09 17:23:34 +0300

    TableView Testings

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -9,16 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     // меняем цвет текста в статус баре, т.к. фон приложения темный
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
 
+    let words = [
+        ["Apple", "Pear", "Watermelon", "Peach", "Mango", "Plum", "Melon", "Fig", "Dates", "Nuts", "Oranges"],
+        ["Carrot", "Pickle", "Potato", "Tomato", "Cucumber", "Ginger", "Cabbage", "Beans"],
+        ["leechee", "Cherry", "Burberry", "Blackberry", "Raspberry"]
+    ]
+    
+    let headers = ["Fruits", "Vegetables", "Berries"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.sectionHeaderHeight = 32
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let arraySection = words[section]
+        return arraySection.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell
+        
+        if let reusedCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reusedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = words[indexPath.section][indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        let alert = UIAlertController(title: nil,
+                                      message: "You have tapped on \(words[indexPath.section][indexPath.row])",
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            alert.dismiss(animated: true)
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
