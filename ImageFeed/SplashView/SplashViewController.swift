03743e9150944a2393b756cfcf0a7cf5edc32d31
Rodion Kim <skyrood2@yandex.ru>
2024-09-17 19:15:37 +0300

    removed potential race condition while getting access token

--- ImageFeed/SplashView/SplashViewController.swift
+++ ImageFeed/SplashView/SplashViewController.swift
@@ -62,11 +62,12 @@ extension SplashViewController: AuthViewControllerDelegate {
     }
     
     private func fetchOAuthToken(_ code: String) {
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
+        
         oauth2Service.fetchOAuthToken(code: code) { [ weak self ] result in
             guard let self = self else { return }
             
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             
             switch result {
             case .success:
