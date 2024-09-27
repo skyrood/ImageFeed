0594f0bd9e44e20a8c0fd920dec4784e93d0c63e
Rodion Kim <skyrood2@yandex.ru>
2024-09-27 16:29:46 +0300

    Moved TabBarController creation to code

---
+++ ImageFeed/TabBarView/TabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+//  Created by Rodion Kim on 27/09/2024.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imageListViewController = storyboard.instantiateViewController(withIdentifier: "ImageListViewController")
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "tab_profile_active"), selectedImage: nil)
+        
+        self.viewControllers = [imageListViewController, profileViewController]
+    }
+}
